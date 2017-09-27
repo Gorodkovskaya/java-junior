b43ff84a78260bc4361c21c028143fffb1dc2d57
Svetlana Gorodkovskaya <sv.gor94@yandex.ru>
2017-09-27 16:55:13 +0300

    Message prefixes as constants

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,14 +1,19 @@
 package com.acme.edu;
 
 public class Logger {
+    private static final String PRIMITIVE = "primitive: ";
+    private static final String CHAR = "char: ";
+
+
     public static void log(int message) {
-        System.out.println("primitive: " + message);
+        System.out.println(PRIMITIVE + message);
     }
 
     public static void log(byte message) {
-        System.out.println("primitive: " + message);
+        System.out.println(PRIMITIVE + message);
     }
+
     public static void log(char message) {
-        System.out.println("char: " + message);
+        System.out.println(CHAR + message);
     }
 }
